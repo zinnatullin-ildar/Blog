7a61ca2c0e1c25145818c227888e7ec693d88592
Ildar Zinnatullin <iqz@izh.com>
2024-02-17 22:03:54 +0400

    Настройка проекта

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Div = styled.div`
+    text-align: center;
+`;
+
+export const App = () => {
+    return (
+        <Div>
+            <i className="fa fa-battery-full fa-2x"></i>
+            <div>123</div>
+        </Div>
+    );
+};
